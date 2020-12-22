2f1760ad27a36a859c84fcc0db29d1a5c69b64b3
RAGHAVENDRA <RAGHAVENDRA@use>
2020-12-22 16:29:20 +0530

    createcon added

---
+++ vtiger2/src/test/java/createorg/Createconta.java
@@ -0,0 +1,14 @@
+package createorg;
+
+public class Createconta {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+       int a=20;
+       int b=30;
+       int c = a+b;
+       System.out.println(c);
+	}
+
+}
