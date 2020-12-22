e77b97786bc7678c68f68e5c30ffed7a812b2f26
RAGHAVENDRA <RAGHAVENDRA@use>
2020-12-22 16:26:12 +0530

    edited

--- vtiger2/src/test/java/createorg/Createorgwithcon.java
+++ vtiger2/src/test/java/createorg/Createorgwithcon.java
@@ -5,7 +5,9 @@ public class Createorgwithcon {
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-System.out.println("jgjbg");
+      System.out.println("jgjbg");
+
 	}
+	///bjmn,n
 
 }
